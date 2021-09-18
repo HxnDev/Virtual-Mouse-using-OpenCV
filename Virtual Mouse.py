6954ea3d669a764bc4b7c5533d9101c22d94f920
Hxn <chhxnshah@gmail.com>
2021-09-18 09:20:14 +0500

    .

--- Virtual Mouse.py
+++ Virtual Mouse.py
@@ -45,3 +45,10 @@ while True:
             if length < 40:
                 cv2.circle(img, (lineInfo[4], lineInfo[5]), 15, (0, 255, 0), cv2.FILLED)
                 autopy.mouse.click()
+
+    cTime = time.time()
+    fps = 1/(cTime-pTime)
+    pTime = cTime
+    cv2.putText(img, str(int(fps)), (20, 50), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 0), 3)
+    cv2.imshow("Image", img)
+    cv2.waitKey(1)
