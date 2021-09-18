2f846a790759aca98deec9000796d06d03bb6c11
Hxn <chhxnshah@gmail.com>
2021-09-18 09:18:31 +0500

    Update

--- Virtual Mouse.py
+++ Virtual Mouse.py
@@ -2,4 +2,13 @@ import cv2
 import numpy as np
 import time
 import HandTracking as ht
-import autopy
+import autopy   # Install using "pip install autopy"
+
+pTime = 0
+width = 640
+height = 480
+frameR = 100
+smoothening = 8
+prev_x, prev_y = 0, 0
+curr_x, curr_y = 0, 0
+
